e7a197e54dde296b3681cf86aa002ce5bf5456f6
Valaragen <rudy.boinnard@gmail.com>
2020-05-06 02:04:20 +0200

    create Reservation model class / issue #1

---
+++ book-api/src/main/java/com/rudy/bibliotheque/mbook/model/Reservation.java
@@ -0,0 +1,27 @@
+package com.rudy.bibliotheque.mbook.model;
+
+import com.rudy.bibliotheque.mbook.model.common.AbstractEntityComposedId;
+import com.rudy.bibliotheque.mbook.model.id.ReservationId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+public class Reservation extends AbstractEntityComposedId {
+
+    @EmbeddedId
+    private ReservationId id;
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date reservationDate;
+
+}
